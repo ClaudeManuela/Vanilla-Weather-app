0153119c77e33ead531e66b117507d09270be350
ClaudeManuela <Manuelaekotto@gmail.com>
2021-02-21 21:35:18 -0700

    Included celcius and Fahrenheit temperature conversions

--- src/index.js
+++ src/index.js
@@ -42,6 +42,8 @@ function showWeather(response) {
     response.data.wind.speed
   )} km/h`;
 
+  Celciustemperature = response.data.main.temp;
+
   document.querySelector("#description").innerHTML = ` ${response.data.weather[0].description}`
   document.querySelector("#humidity").innerHTML = `Humidity: ${response.data.main.humidity}%`
   document.querySelector("#feels-like").innerHTML = `Feels like:  ${response.data.main.feels_like}°C`
@@ -76,5 +78,27 @@ button.addEventListener("submit", getTemp);
 
 let currentbutton = document.querySelector("#current");
 currentbutton.addEventListener("click", getPosition);
+
+// for converting celcius to Fahrenheit and vice versa
+let Celciustemperature = null;
+
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (Celciustemperature * 9)/5 +32;
+  document.querySelector("#temperature").innerHTML = Math.round(fahrenheitTemperature) 
+}
+
+let fahrenheittemp= document.querySelector("#fahrenheit-temp");
+fahrenheittemp.addEventListener("click", displayFahrenheitTemperature)
+
+function displayCelciusTemperature (event) {
+  event.preventDefault();
+  let CelciusElement = document.querySelector("#temperature")
+  CelciusElement.innerHTML = Math.round(Celciustemperature)
+}
+
+let celciustemp = document.querySelector("#celcius-temp");
+celciustemp.addEventListener("click", displayCelciusTemperature)
 
 search("Tokyo");
