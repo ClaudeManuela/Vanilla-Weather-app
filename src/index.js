9bed5082b9843ee06f0a078312c548061d3cb334
ClaudeManuela <Manuelaekotto@gmail.com>
2021-02-21 23:32:06 -0700

    Completed the full days forecast in html and js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-let now = new Date();
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
 
 let days = [
   "Sunday",
@@ -10,30 +11,63 @@ let days = [
   "Saturday"
 ];
 let day = days[now.getDay()];
-let hour = now.getHours();
-
-if (hour < 10) {
-  hour = `0${hour}`;
+return `${day} ${formatHours(timestamp)}`;
 }
 
-let minutes = now.getMinutes();
+function formatHours(timestamp) {
+  let now = new Date(timestamp);
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hour}:${minutes}`; }
 
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+function displayForecast(response){
+
+  let forecastElement= document.querySelector("#forecast");
+  let forecast = null;
+  forecastElement.innerHTML = null;
+
+  for (let index = 0; index < 6; index++) {
+    let forecast = response.data.list[index];
+
+    forecastElement.innerHTML += 
+     `
+     <div class="row"> 
+     <div class="col-2">
+        <div class="days" style="width: 8rem;">
+      
+            <div>
+                ${formatHours(forecast.dt * 1000)}
+                <br />
+                  <div class=forecast-temp>
+                      <strong> ${Math.round(forecast.main.temp_max)}° | </strong> ${Math.round(forecast.main.temp_min)}°
+                      <br />
+                      <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png">
+                    <div>
+              </div>
+          </div>
+        </div>
+        </div>`;
+    
+  }
 }
 
-let currentDate = document.querySelector("#today-date");
-
-currentDate.innerHTML = `Today: ${day} ${hour}:${minutes}`;
 
 function search(city) {
   let apiKey = "53a928a54f9cfac5d9a2b1324d145d62";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showWeather(response) {
-  console.log(response)
   document.querySelector("#input-city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -41,13 +75,14 @@ function showWeather(response) {
   document.querySelector("#wind").innerHTML = `Wind: ${Math.round(
     response.data.wind.speed
   )} km/h`;
-
   Celciustemperature = response.data.main.temp;
 
   document.querySelector("#description").innerHTML = ` ${response.data.weather[0].description}`
   document.querySelector("#humidity").innerHTML = `Humidity: ${response.data.main.humidity}%`
   document.querySelector("#feels-like").innerHTML = `Feels like:  ${response.data.main.feels_like}°C`
+  document.querySelector("#today-date").innerHTML = `Today: ${formatDate(response.data.dt * 1000)}`;
   document.querySelector("#icon").setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+
 }
 
 function randomCity(event) {
